47bdc7b7ea65bd9c61a21db659b50b55235f786b
Oleggdl <oblotskiyj@gmail.com>
2021-08-07 19:57:30 +0300

    feat: solved the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+  let result='';
+  n=Math.abs(n)
+  while(n){
+     
+        result= result+ n%10;
+        n=Math.floor(n/10);
+
+  }
+  return result
 }
